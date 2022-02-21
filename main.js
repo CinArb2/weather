ff8691722e2ce9f8391157714bf5d0219feab837
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2022-02-21 12:19:07 -0500

    proxy

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
 const apiKey = '9b17d7be3dc82eaf6d86688e7d03c770';
+const proxy = 'https://weather-app-cindy-arb.herokuapp.com/'
 
 const url = city => {
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 }
 
 async function getWeatherByLocation(city) {
-    const resp = await fetch(url(city));
+    const resp = await fetch(url(city), { origin: "cors"});
     //const respData = await resp.json();
 
     console.log(resp);
