0cd883b10a7c53091e07c33b40bd2f5435b78dea
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2022-02-22 16:38:39 -0500

    finalizado weather app

--- main.js
+++ main.js
@@ -1,61 +1,111 @@
 
 const apiKey = '9b17d7be3dc82eaf6d86688e7d03c770';
-let city = '';
-let nameCity = document.querySelector('[name="city"]')
-let weatherInfo = document.querySelector('.weather_info')
-let urlBack = `https://imsea.herokuapp.com/api/1?q=`
-let proxy = `https://cors-anywhere.herokuapp.com/`
+let nameCity = document.querySelector('[name="city"]');
+let weatherInfo = document.querySelector('.weather_info');
+
 
 const url = (city) => {
-  return `https://api.openweathermap.org/data/2.5/find?q=${city}&cnt=10&appid=${apiKey}`
+  if (typeof city === 'object') {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.long}&appid=${apiKey}`
+  } else {
+    return `https://api.openweathermap.org/data/2.5/find?q=${city}&cnt=10&appid=${apiKey}`
+  }
 }
 
-function createEl(city, temp, state, iconId) {
+function createEl(data) {
   let elemento = document.createElement('div')
   elemento.classList.add('temp-container');
+  let temp = data.main.temp * 1 - 273.15;
 
-  elemento.innerHTML = `<h1 class="city">${city}</h1>
+  elemento.innerHTML = 
+        `<img class="icon_weather" src="./incons/icon-weat/${data.weather[0].icon}.png" alt="icon weather state">
+        <h1 class="city">${data.name}</h1>
+        <button class="temperature">${temp.toFixed(2)}°C</button>
+        <span class="weather_states">${data.weather[0].main}</span>
         <div class="temp_info">
-          <span class="temperature">${temp}°F</span>
-          <span class="weather_states">${state}</span>
-          <img src="http://openweathermap.org/img/wn/${iconId}@2x.png" alt="icon weather state">
+          <div class="box wind">
+            <p>Wind</p>
+            <span>${data.wind.speed}m/s</span>
+          </div>
+          <div class="box hum">
+            <p>Hum</p>
+            <span>${data.main.humidity}%</span>
+          </div>
         </div>`
   weatherInfo.appendChild(elemento);
 }
 
-
-async function getWeatherByLocation(city) {
-  const resp = await fetch(url(city), { origin: "cors" });
-  
-  const respData = await resp.json();
-
-  let cityName = respData.list[0].name
-  let temperature = respData.list[0].main.temp;
-  let state = respData.list[0].weather[0].description;
-  let iconId = respData.list[0].weather[0].icon;
-  
-  createEl(cityName, temperature, state, iconId)
-  
-  const imgback = await fetch(`${proxy}${urlBack}${cityName}`, { origin: "cors" });
-  const respImg = await imgback.json();
-
-  const backImage = respImg.results[0];
-  weatherInfo.style.backgroundImage = `url(${backImage})`;
+function notFound() {
+  let elemento = document.createElement('div');
+    elemento.classList.add('temp-container');
+  elemento.innerHTML = `
+    <img class="icon_not-found" src="./incons/Attention_perspective_matte.png" alt="">
+    <h1 class="city">Ooops...</h1>
+    <p>City not found</p>
+    <span>Please try again .. :)</span>  
+  `
+    weatherInfo.appendChild(elemento);
 }
 
+async function getWeatherByLocation(city) {
+  try {
+    const weather = await fetch(url(city), { origin: "cors" });
+    const weatherData = await weather.json();
 
-nameCity.addEventListener('keydown', e => {
-
-  let tempContainer = document.querySelector('.temp-container');
-
-  if (e.key === 'Enter') {
-
-    city = nameCity.value;
-
-    if (tempContainer) {
-      tempContainer.remove();
+    if (weatherData.list) {
+      createEl(weatherData.list[0]);
+    } else {
+      createEl(weatherData);
     }
-    getWeatherByLocation(city);
+  } catch (error) {
+    notFound()
   }
+}
+
+const successCb = (position) => {
+  let objCoord = {
+    lat: position.coords.latitude,
+    long: position.coords.longitude,
+  }
+  getWeatherByLocation(objCoord);
+}
+
+const convertTemp = (elem) => {
+  const numberPattern = /[-]?[0-9]+[.]?[0-9]+/g;
+  let number = elem.match(numberPattern);
+  
+  if (elem.includes('F')) {
+    let cels = (number * 1 - 32) * (5 / 9);
+    return cels.toFixed(2)
+  } else if (elem.includes('C')) {
+    let far = (number * 9 / 5) + 32;
+    return far.toFixed(2)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  navigator.geolocation.getCurrentPosition(successCb);
+
+  nameCity.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+      let tempContainer = document.querySelector('.temp-container');
+      if (tempContainer) {tempContainer.remove()};
+      getWeatherByLocation(nameCity.value);
+    }
+  })
+
+  document.addEventListener('click', e => {
+    if (e.target.matches('.temperature') && e.target.textContent.includes('F')) {
+      let cels = convertTemp(e.target.textContent);
+      e.target.textContent = `${cels}°C`;
+
+    } else if (e.target.matches('.temperature') && e.target.textContent.includes('C')) {
+      let farh = convertTemp(e.target.textContent);
+      e.target.textContent = `${farh}°F`;
+    }
+  });
+
 })
+
+
 
